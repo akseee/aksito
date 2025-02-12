81a590df9cc872fcd5076545b86fc7c624a35e0c
Akseenja <klasik1995@bk.ru>
2025-02-12 23:37:40 +0300

    feat: add types

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -4,3 +4,40 @@ export enum RequestStatus {
   FAILED = "error",
   LOADING = "loading",
 }
+
+// REAL_ESTATE: "Недвижимость",
+// AUTO: "Авто",
+// SERVICES: "Услуги",
+
+export type categoryType = "Недвижимость" | "Авто" | "Услуги";
+
+export type ItemType = {
+  name: string;
+  description: string;
+  location: string;
+  type: categoryType;
+  image?: string;
+};
+
+export type EstateType = {
+  propertyType: string;
+  area: number;
+  rooms: number;
+  price: number;
+};
+
+export type AutoType = {
+  brand: string;
+  model: string;
+  year: number;
+  mileage?: number;
+};
+
+export type ServiceType = {
+  serviceType: string;
+  experience: number;
+  cost: number;
+  workSchedule?: string;
+};
+
+export type Id = number;
