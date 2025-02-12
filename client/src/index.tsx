ff8d969c5cba3a9a48e725c0a9b368551fc167b0
Akseenja <klasik1995@bk.ru>
2025-02-13 00:32:57 +0300

    chore: delete redux & redux@toolkit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,9 +4,7 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 
-import store from "./services/store";
 import App from "./components/app/app";
 
 const container = document.getElementById("root") as HTMLElement;
@@ -15,9 +13,7 @@ const root = ReactDOMClient.createRoot(container!);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App></App>
-      </Provider>
+      <App></App>
     </BrowserRouter>
   </React.StrictMode>
 );
