0a16aa878f33735fe900e892d29530fad982559c
Akseenja <klasik1995@bk.ru>
2025-02-18 02:56:15 +0300

    feat: add protected route

---
+++ client/src/utils/ProtectedRoute.tsx
@@ -0,0 +1,36 @@
+import { Preloader } from "@ui";
+import { useContext } from "react";
+
+import { Navigate, useLocation } from "react-router";
+import { UserContext } from "src/context/UserContext";
+
+type ProtectedRouteProps = {
+  onlyUnAuth?: boolean;
+  children: React.ReactElement;
+};
+
+export const ProtectedRoute = ({
+  onlyUnAuth,
+  children,
+}: ProtectedRouteProps) => {
+  const context = useContext(UserContext);
+
+  const location = useLocation();
+  const { isAuthChecked, data } = context;
+
+  if (!isAuthChecked) {
+    return <Preloader />;
+  }
+
+  if (!onlyUnAuth && !data) {
+    return <Navigate replace to="/login" state={{ from: location }} />;
+  }
+
+  if (onlyUnAuth && data) {
+    const from = location.state?.from || { pathname: "/" };
+
+    return <Navigate replace to={from} />;
+  }
+
+  return children;
+};
