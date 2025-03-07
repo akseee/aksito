6e7d753734b955eecad5f0deedecab1a60051c0d
Akseenja <klasik1995@bk.ru>
2025-03-08 02:05:19 +0300

    feat: create hook for login

---
+++ client/src/hooks/useLogin.ts
@@ -0,0 +1,35 @@
+import { useMutation } from "@tanstack/react-query";
+import { useContext } from "react";
+import { api } from "src/api/api";
+import { UserContext } from "src/context/UserContext";
+import { TLoginForm, TUserType } from "src/utils/types";
+
+type TLoginResponse = {
+  message: string;
+  token: string;
+  user: TUserType;
+};
+
+const loginUser = async (loginData: TLoginForm) => {
+  const response = await api.post<TLoginResponse>("/users/login", loginData);
+  return response.data;
+};
+
+export function useLogin() {
+  const context = useContext(UserContext);
+
+  if (!context) {
+    throw new Error("no context found");
+  }
+  const { login } = context;
+
+  const { mutate, error } = useMutation({
+    mutationKey: ["login user"],
+    mutationFn: loginUser,
+    onSuccess: (data: TLoginResponse) => {
+      login(data.token);
+    },
+  });
+
+  return { mutate, error };
+}
--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,37 +1,12 @@
-import { FC, useContext } from "react";
+import { FC } from "react";
 import styles from "./login.module.css";
 import { ContentWrapper } from "@ui";
-import { UserContext } from "src/context/UserContext";
-import { useMutation } from "@tanstack/react-query";
 import { TLoginForm } from "src/utils/types";
-import { api } from "src/api/api";
 import { LoginForm } from "@components";
+import { useLogin } from "src/hooks/useLogin";
 
 export const LoginPage: FC = () => {
-  const { mutate, error, isError } = useMutation({
-    mutationKey: ["login user"],
-    mutationFn: async (loginData: TLoginForm) => {
-      const response = await api.post<{ token: string }>(
-        "/users/login",
-        loginData
-      );
-      return response.data;
-    },
-    onSuccess: (data) => {
-      login(data.token);
-    },
-    onError: (data) => {
-      console.log(data);
-    },
-  });
-
-  const context = useContext(UserContext);
-
-  if (!context) {
-    throw new Error("no context found");
-  }
-
-  const { login } = context;
+  const { mutate, error } = useLogin();
 
   const onSubmit = (user: TLoginForm) => {
     mutate(user);
@@ -40,10 +15,7 @@ export const LoginPage: FC = () => {
   return (
     <ContentWrapper title="Вход" extraClass={styles.box}>
       <h2 className={styles.title}>Войти в аккаунт</h2>
-      <LoginForm
-        submitForm={onSubmit}
-        error={isError ? error : null}
-      ></LoginForm>
+      <LoginForm submitForm={onSubmit} error={error}></LoginForm>
       <p style={{ opacity: 0.2 }}>
         OR: password: "123123", email: "test@example.com",
       </p>
