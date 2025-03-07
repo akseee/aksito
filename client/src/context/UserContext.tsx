9039263cdd982916aa2e16c47724af382f47909a
Akseenja <klasik1995@bk.ru>
2025-03-07 22:54:31 +0300

    refactor: separate provider and context

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,5 +1,4 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
-import { jwtDecode } from "jwt-decode";
+import { createContext } from "react";
 
 type InitialState = {
   isAuthChecked: boolean;
@@ -8,56 +7,9 @@ type InitialState = {
   logout: () => void;
 };
 
-type contextProps = {
-  children: ReactNode;
-};
-
-const UserContext = createContext<InitialState>({
+export const UserContext = createContext<InitialState>({
   isAuthChecked: false,
   isAuthenticated: false,
   login: () => {},
   logout: () => {},
 });
-
-const UserProvider = ({ children }: contextProps) => {
-  const [isAuthChecked, setIsAuthChecked] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem("authToken");
-
-    if (token) {
-      try {
-        const decoded = jwtDecode<{ exp: number }>(token);
-        if (decoded.exp * 1000 < Date.now()) {
-          throw new Error("Token expired");
-        }
-        setIsAuthenticated(true);
-      } catch (err) {
-        console.log(err);
-        localStorage.removeItem("authToken");
-      }
-    }
-    setIsAuthChecked(true);
-  }, []);
-
-  const login = (token: string) => {
-    localStorage.setItem("authToken", token);
-    setIsAuthenticated(true);
-  };
-
-  const logout = () => {
-    localStorage.removeItem("authToken");
-    setIsAuthenticated(false);
-  };
-
-  return (
-    <UserContext.Provider
-      value={{ isAuthChecked, isAuthenticated, login, logout }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
-};
-
-export { UserContext, UserProvider };
