57217b3a52d0cc423a498f1a517540149c64275e
Akseenja <klasik1995@bk.ru>
2025-03-10 22:58:14 +0300

    refactor: change abck butyton for wrapper

--- client/src/components/ui/content-wrapper/content-wrapper.tsx
+++ client/src/components/ui/content-wrapper/content-wrapper.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from "react";
 import styles from "./content-wrapper.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import clsx from "clsx";
 
 type ContentWrapperProps = {
@@ -16,14 +16,21 @@ export const ContentWrapper: FC<ContentWrapperProps> = ({
   children,
   button = false,
   extraClass,
-}) => (
-  <main className={styles.wrapper}>
-    <div className={clsx(styles.header)}>
-      {button && (
-        <NavLink to="/" className={styles.back}>{`← на главную`}</NavLink>
-      )}
-      <h2 className={styles.title}>{title}</h2>
-    </div>
-    <div className={clsx(styles.content, extraClass)}>{children}</div>
-  </main>
-);
+}) => {
+  const navigate = useNavigate();
+  return (
+    <main className={styles.wrapper}>
+      <div className={clsx(styles.header)}>
+        {button && (
+          <NavLink
+            to="#"
+            className={styles.back}
+            onClick={() => navigate(-1)}
+          >{`← назад`}</NavLink>
+        )}
+        <h2 className={styles.title}>{title}</h2>
+      </div>
+      <div className={clsx(styles.content, extraClass)}>{children}</div>
+    </main>
+  );
+};
